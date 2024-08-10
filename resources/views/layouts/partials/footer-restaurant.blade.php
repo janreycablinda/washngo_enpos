44c3174a561d20a50882bd7321d41ce1eba83448
Janrey Cablinda <janrey@pmmalliance.com>
2024-08-10 19:24:44 +0800

    first commit

---
+++ resources/views/layouts/partials/footer-restaurant.blade.php
@@ -0,0 +1,12 @@
+<!-- Main Footer -->
+  <footer class="no-print text-center text-info">
+    <!-- To the right -->
+    <!-- <div class="pull-right hidden-xs">
+      Anything you want
+    </div> -->
+    <!-- Default to the left -->
+    <small>
+    	<b>{{ config('app.name', 'ultimatePOS') }} - V{{config('author.app_version')}} | Copyright &copy; {{ date('Y') }} All rights reserved.</b>
+      <span class="sr-only">| Coded by <a href="https://www.linkedin.com/in/khalil-minalang-47a0a6188/">Khalil Abdulrahman Minalang</a></span>
+    </small>
+</footer>
---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,18 @@
+<!-- Main Footer -->
+  <footer class="main-footer no-print">
+    <!-- To the right -->
+    <!-- <div class="pull-right hidden-xs">
+      Anything you want
+    </div> -->
+    <!-- Default to the left -->
+    <small>
+    	{{ config('app.name', 'ultimatePOS') }} - V{{config('author.app_version')}} | Copyright &copy; {{ date('Y') }} All rights reserved.
+      <span class="sr-only">| Coded by <a href="https://www.linkedin.com/in/khalil-minalang-47a0a6188/">Khalil Abdulrahman Minalang</a></span>
+    </small>
+    <div class="btn-group pull-right">
+      	<button type="button" class="btn btn-success btn-xs toggle-font-size" data-size="s"><i class="fa fa-font"></i> <i class="fa fa-minus"></i></button>
+      	<button type="button" class="btn btn-success btn-xs toggle-font-size" data-size="m"> <i class="fa fa-font"></i> </button>
+      	<button type="button" class="btn btn-success btn-xs toggle-font-size" data-size="l"><i class="fa fa-font"></i> <i class="fa fa-plus"></i></button>
+      	<button type="button" class="btn btn-success btn-xs toggle-font-size" data-size="xl"><i class="fa fa-font"></i> <i class="fa fa-plus"></i><i class="fa fa-plus"></i></button>
+    </div>
+</footer>
